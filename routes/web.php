3ae6ab9a741329258a815c826cdf5ce2898d8e7f
Stylianos Nikopoulos <nikstel2001@gmail.com>
2024-09-19 19:58:35 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,20 @@ Route::get('/products', [ProductController::class, 'index'])->name('products');
 Route::get('/about', [ProductController::class, 'about'])->name('about');
 
 // Cart routes
-Route::get('/cart/add', [CartController::class, 'addToCart'])->name('addToCart')->middleware('auth:sanctum');;
+Route::get('/cart/add', [CartController::class, 'addToCart'])->name('addToCart')->middleware('auth:sanctum');
 Route::get('/cart', [CartController::class, 'getCartItems'])->middleware('auth:sanctum')->name('getCartItems')->middleware('auth:sanctum');
 Route::get('/cart/items', [CartController::class, 'getCartItems'])->middleware('auth:sanctum');
 Route::delete('/cart/items/{productId}', [CartController::class, 'deleteProduct'])->middleware('auth:sanctum');
 Route::patch('/cart/items/{productId}', [CartController::class, 'updateQuantity'])->middleware('auth:sanctum');
-Route::get('/cart/checkout', [CartController::class, 'checkout'])->name('checkout')->middleware('auth:sanctum');;
+Route::get('/cart/checkout', [CartController::class, 'checkout'])->name('checkout')->middleware('auth:sanctum');
 Route::get('/cart', function () {
     return view('cart'); 
 })->name('cart')->middleware('auth:sanctum');;
 
 // Stripe routes
-Route::post('/api/stripe/create-checkout-session', [StripeController::class, 'createCheckoutSession'])->middleware('auth:sanctum');;
-Route::get('/stripe/success', [StripeController::class, 'success'])->name('payment-success')->middleware('auth:sanctum');;
-Route::get('/stripe/cancel', [StripeController::class, 'cancel'])->name('payment-cancelled')->middleware('auth:sanctum');;
+Route::post('/api/stripe/create-checkout-session', [StripeController::class, 'createCheckoutSession'])->middleware('auth:sanctum');
+Route::get('/stripe/success', [StripeController::class, 'success'])->name('payment-success')->middleware('auth:sanctum');
+Route::get('/stripe/cancel', [StripeController::class, 'cancel'])->name('payment-cancelled')->middleware('auth:sanctum');
 
 
 
